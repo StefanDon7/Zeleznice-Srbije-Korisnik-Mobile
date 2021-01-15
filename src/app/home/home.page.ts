801cd6bfe9fe7bff7222db89dcb215f408f17ac2
StefanDon7 <s.lezaic95@gmail.com>
2021-01-16 00:40:20 +0100

    validacija za registraciju i prijavljivanje

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,45 +1,52 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { CookieService } from 'angular2-cookie/services/cookies.service';
-import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
-import { Klijent } from '../models/klijent.model';
-import { HomeService } from './home.service';
-
-
-
-
-
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { CookieService } from "angular2-cookie/services/cookies.service";
+import { HomeService } from "./home.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(
+    private homeService: HomeService,
+    private router: Router,
+    private _cookieService: CookieService,
+    private alertController: AlertController
+  ) {}
+  klijent: any;
 
-  constructor(private homeService:HomeService,private router: Router,private _cookieService:CookieService,private alertController:AlertController) {
-    
+  signIn(form: NgForm) {
+    if(!this.proveriPolja(form)){
+      this.vratiPoruku("Paznja","","Sva polja moraju biti popunjena!")
+    }else{ 
+    this.homeService
+      .signin(form.value.email, form.value.password)
+      .subscribe((data) => {
+        this.klijent = data;
+        this.prijaviKlijenta();
+        form.resetForm();
+      });
     }
-  klijent:any;
-  
-
-   signIn(form:NgForm){
-    this.homeService.signin(form.value.email,form.value.password).subscribe(data=>{
-      this.klijent=data;
-      this.prijaviKlijenta();
-      form.resetForm();
-    })
-    
   }
-   prijaviKlijenta(){
-    if(this.klijent!=null){
-      sessionStorage.setItem("klijent",this.klijent.id)
-      this.vratiPoruku("Uspesno prijavljivanje","Dobro dosli","Korisnik: "+this.klijent.ime+" "+this.klijent.prezime)
-      this.router.navigate(['/main'])
-    }else{
-      this.vratiPoruku("Paznja","Pogresni parameteri","Proverite da li ste dobro uneili vase podatke!")
+  prijaviKlijenta() {
+    if (this.klijent != null) {
+      sessionStorage.setItem("klijent", this.klijent.id);
+      this.vratiPoruku(
+        "Uspesno prijavljivanje",
+        "Dobro dosli",
+        "Korisnik: " + this.klijent.ime + " " + this.klijent.prezime
+      );
+      this.router.navigate(["/main"]);
+    } else {
+      this.vratiPoruku(
+        "Paznja",
+        "Pogresni parameteri",
+        "Proverite da li ste dobro uneili vase podatke!"
+      );
     }
   }
 
@@ -52,4 +59,13 @@ export class HomePage {
     });
     await alert.present();
   }
+  proveriPolja(form: NgForm) {
+    if (form.value.email == "" || form.value.email == null) {
+      return false;
+    } else if (form.value.password == "" || form.value.password == null)
+      return false;
+    else {
+      return true;
+    }
   }
+}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -21,12 +21,20 @@ export class RegisterPage implements OnInit {
   ngOnInit() {}
 
   register(form: NgForm) {
+    if(!this.proveriPolja(form)){
+      this.vratiPoruku("Paznja","","Sva polja moraju biti popunjena!")
+    }else if(!this.lozinkeSeNePoklapaju(form)){
+      this.vratiPoruku("Paznja","","Lozinke se ne podudaraju!")
+    }
+    else{
     this.proveriKorisnika(form.value.email);
     if (this.email != null) {
       this.vratiPoruku(
         "Paznja",
         "",
-        "Nalog sa email adresom: " + form.value.email + " vec postoji u sistemu!"
+        "Nalog sa email adresom: " +
+          form.value.email +
+          " vec postoji u sistemu!"
       );
       form.resetForm();
     } else {
@@ -44,14 +52,12 @@ export class RegisterPage implements OnInit {
         });
     }
     form.resetForm();
-    this.router.navigate(['/home'])
-
+    this.router.navigate(["/home"]);
+  }
   }
   proveriKorisnika(email: string) {
     this.registerService.vratiKorisnika(email).subscribe((data) => {
-     
       this.email = data;
-     
     });
   }
   registracijaKlijenta() {
@@ -77,5 +83,31 @@ export class RegisterPage implements OnInit {
       buttons: ["Ok"],
     });
     await alert.present();
+  }
+
+  proveriPolja(form: NgForm) {
+    if (form.value.email == "" || form.value.email == null) {
+      return false;
+    } else if (form.value.email == "" || form.value.email == null) {
+      return false;
+    } else if (form.value.username == "" || form.value.username == null) {
+      return false;
+    } else if (form.value.firstname == "" || form.value.firstname == null) {
+      return false;
+    }else if (form.value.lastname == "" || form.value.lastname == null) {
+      return false;
+    }else if (form.value.password == "" || form.value.password == null) {
+      return false;
+    }else if (form.value.repassword == "" || form.value.repassword == null) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+  lozinkeSeNePoklapaju(form: NgForm){
+    if(form.value.password!=form.value.repassword){
+      return false;
+    }
+    return true;
   }
 }
